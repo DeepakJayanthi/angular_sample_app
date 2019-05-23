a693f551bb7a44e8ab95703c674a3a442af35cb6
DeepakJayanthi <jayanthideepak596@gmail.com>
2019-05-22 21:17:25 -0500

    Added changes

--- app/app.routing.js
+++ app/app.routing.js
@@ -13,5 +13,8 @@ angular.module("myApp").config(function ($routeProvider) {
     .when("/pmbok", {
         templateUrl : "app/components/pmbok/pmbokmatrix.html",
         controller : "matrixctrl"
+    }).when("/university", {
+        templateUrl : "app/components/university/university.html",
+        controller : "univctrl"
     });
 });
